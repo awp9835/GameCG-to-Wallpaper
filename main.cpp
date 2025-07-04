51e4d607819afe37a75f25e368eaec18685f0fed
AWPSOFT <awp9835@outlook.com>
2025-07-04 21:15:53 +0800

    修复了切换任务视图导致壁纸设置失灵问题

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "Shlobj.h"
 #include "windows.h"
 #include <vector>
+#define SET_WALLPAPER_HOTKEY VK_LCONTROL
+#define RESET_WALLPAPER_HOTKEY VK_RCONTROL 
+#define TASK_VIEW_COMPATIBILITY true
+#define SET_KEY_CHECK_INTERVAL 10
+#define STATE_CHECK_INTERVAL 100
+#define RETRY_DELAY_MS 3000
+#define RETRY_TIMES 3
+
 //将动画窗口设为壁纸窗口，用于实现动态壁纸。
 //成功：返回被隐藏的一个系统窗口。
 //失败：返回NULL
-HWND SetAsWallpaperWindow(HWND hwnd, bool autoFullScreen = false)
+HWND SetAsWallpaperWindow(HWND hwnd, bool autoFullScreen = false, bool taskViewCompatibility = TASK_VIEW_COMPATIBILITY)
 {
     //Step1 开启自动切换壁纸，否则无法分离桌面图标和壁纸
     //main需要先 CoInitializeEx
@@ -33,21 +41,32 @@ HWND SetAsWallpaperWindow(HWND hwnd, bool autoFullScreen = false)
                 }
                 item->Release();
         }
-        Sleep(100); //首次设置可能失败，所以延迟0.1s
         paper->Release();
     }
     HWND deskhwnd = 0,wpaperhwnd = 0,iconshwnd = 0;
     //寻找最后两个WorkerW
-    while (true)
-    {
-        deskhwnd = FindWindowExW(0, deskhwnd, L"WorkerW", L"");
-        if (deskhwnd == 0) break;
-        iconshwnd = wpaperhwnd;
-        wpaperhwnd = deskhwnd;
-    }
-    //检验结果是否正确
-    if (wpaperhwnd == 0 || iconshwnd == 0) return 0;
-    else if (0 == FindWindowExW(iconshwnd, 0, L"SHELLDLL_DefView", L"")) return 0;
+    
+    for (int i = 0; i < RETRY_TIMES; i++)
+    { 
+        while (true)
+        {
+            deskhwnd = FindWindowExW(0, deskhwnd, L"WorkerW", L"");
+            if (deskhwnd == 0) break;
+            iconshwnd = wpaperhwnd;
+            wpaperhwnd = deskhwnd;
+        }
+        //检验结果是否正确，否则等3秒钟再试一次
+        //首次设置可能失败，所以延迟
+        if (wpaperhwnd == 0 || iconshwnd == 0)
+        {
+            Sleep(RETRY_DELAY_MS);
+            continue;
+        }
+        else break;
+    } 
+
+    HWND defviewhwnd = FindWindowExW(iconshwnd, 0, L"SHELLDLL_DefView", L"");
+    if (defviewhwnd == 0) return 0;
     deskhwnd = FindWindowExW(0, wpaperhwnd, L"Progman", L"Program Manager");//寻找放置位置
     if (deskhwnd == 0) return 0;
     SetParent(hwnd, deskhwnd);
@@ -59,6 +78,21 @@ HWND SetAsWallpaperWindow(HWND hwnd, bool autoFullScreen = false)
         //PostMessage(hwnd, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
     } 
     ShowWindow(wpaperhwnd, SW_HIDE); //隐藏系统的上层壁纸
+    if(TASK_VIEW_COMPATIBILITY)
+    {
+        while (GetAsyncKeyState(RESET_WALLPAPER_HOTKEY) >= 0)
+        {
+            //使用任务视图后，出现Bug，是因为窗口Z序发生了变化，需要重置
+            //GetParent无法正常使用，返回NULL，必须使用其他方法
+            HWND znext = GetWindow(hwnd, GW_HWNDNEXT);
+            if (znext == defviewhwnd) //if(znext != 0)
+            { 
+                SetParent(hwnd, deskhwnd);  
+            }
+            ShowWindow(wpaperhwnd, SW_HIDE);
+            Sleep(STATE_CHECK_INTERVAL);
+        }
+    }
     return wpaperhwnd;
 }
 int main(int argc, char** argv)
@@ -75,17 +109,30 @@ int main(int argc, char** argv)
     else
     {
         //请切换到指定窗口，按左ctrl开启，在此之前，需要打开本程序，并且使游戏按F11全屏
-        printf("请切换到指定窗口，开启全屏模式，按左Ctrl即可将其设置为桌面壁纸\n");
-        while (GetAsyncKeyState(VK_LCONTROL) >= 0) Sleep(10); 
+        if (SET_WALLPAPER_HOTKEY == VK_LCONTROL)        
+            printf("请切换到指定窗口，开启全屏模式，长按左Ctrl即可将其设置为桌面壁纸\n");
+        else 
+            printf("请切换到指定窗口，开启全屏模式，长按自定义的SET_WALLPAPER_HOTKEY即可将其设置为桌面壁纸\n");
+
+        while (GetAsyncKeyState(SET_WALLPAPER_HOTKEY) >= 0) Sleep(SET_KEY_CHECK_INTERVAL);
         hwnd = GetForegroundWindow();
+        printf("---------------------------------------------------\n");
+        printf("已尝试将指定窗口设置为壁纸，如未成功请重新运行本程序\n");
+        printf("---------------------------------------------------\n");
+        if (RESET_WALLPAPER_HOTKEY == VK_RCONTROL)
+            printf("若要还原，请长按右Ctrl，并将原初壁纸置于当前程序所在目录\n");
+        else
+            printf("若要还原，请长按自定义的RESET_WALLPAPER_HOTKEY，并将原初壁纸置于当前程序所在目录\n");
+        if (!TASK_VIEW_COMPATIBILITY)  while (GetAsyncKeyState(RESET_WALLPAPER_HOTKEY) >= 0) Sleep(STATE_CHECK_INTERVAL);
+        else printf("---------------------------------------------------\n请将本控制台移到其他任务视图\n");
         SetAsWallpaperWindow(hwnd, false);
     }
-    printf("已尝试将指定窗口设置为壁纸\n");
-    //按右ctrl还原窗口，并使用当前目录的壁纸，也可以不还原直接退出。
-    printf("若要还原，请按右Ctrl，并将原初壁纸置于当前程序所在目录\n");
-    while (GetAsyncKeyState(VK_RCONTROL) >= 0) Sleep(10); 
-    SetParent(hwnd, NULL);
 
+    //还原窗口，并使用当前目录的壁纸，也可以不还原直接退出。
+
+    SetParent(hwnd, NULL);
+    printf("---------------------------------------------------\n");
+    printf("已退出，请重新设置壁纸\n");
     CoUninitialize();
     CoUninitialize();
     return 0;
